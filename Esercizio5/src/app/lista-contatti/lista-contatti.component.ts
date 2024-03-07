2382d88a3250b2d1a7e60aa6f899c9af31496fde
astorelloo <149362424+astorelloo@users.noreply.github.com>
2024-03-07 23:02:50 +0100

    Update lista-contatti.component.ts

--- Esercizio5/src/app/lista-contatti/lista-contatti.component.ts
+++ Esercizio5/src/app/lista-contatti/lista-contatti.component.ts
@@ -11,15 +11,15 @@ import { shareReplay } from 'rxjs';
 })
 export class ListaContattiComponent {
   persone = [
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nome: "Luca ", cognome: "Azzo", eta: "22", nTelefono: "+39 347 810 4148", stato:true, colore: "Rosso"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nome: "Eddy", cognome: "Okane", eta: "25", nTelefono: "111111111", stato:false, colore: "Verde"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nome: "Three ", cognome: "Mone", eta: "69", nTelefono: "22222222", stato: true, colore: "Blu"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nome: "Mike ", cognome: "Coxlong", eta: "34", nTelefono: "02 550271", stato:false, colore: "Giallo"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nome: "Gabe ", cognome: "Itch", eta: "17", nTelefono: "118", stato: true, colore: "Viola"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nome: "Dixie " , cognome: "Normus", eta: "18", nTelefono: "1522", stato:false, colore: "Ciano"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nome: "Antonio", cognome: "Soliman", eta: "15", nTelefono: "+39 348 968 1247", stato:false, colore: "Marrone"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nome: "Lorenzo " , cognome: "Hu", eta: "18", nTelefono: "+39 347 810 4148", stato:true, colore: "Verde scuro"},
-    {immagine: "https://www.fotorapida.com/wp-content/uploads/2018/02/giusta-e1582735572290.jpg", nome: "Thomas ", cognome: "Turbato", eta: "20", nTelefono: "420", stato:false, colore: "f0cd23a"}
+    {immagine: "https://i.ytimg.com/vi/M2jVRUH2JUA/maxresdefault.jpg", nome: "Luca ", cognome: "Azzo", eta: "22", nTelefono: "+39 347 810 4148", stato:true, colore: "Rosso"},
+    {immagine: "https://i.ytimg.com/vi/M2jVRUH2JUA/maxresdefault.jpg", nome: "Eddy", cognome: "Okane", eta: "25", nTelefono: "111111111", stato:false, colore: "Verde"},
+    {immagine: "https://i.ytimg.com/vi/M2jVRUH2JUA/maxresdefault.jpg", nome: "Three ", cognome: "Mone", eta: "69", nTelefono: "22222222", stato: true, colore: "Blu"},
+    {immagine: "https://i.ytimg.com/vi/M2jVRUH2JUA/maxresdefault.jpg", nome: "Mike ", cognome: "Coxlong", eta: "34", nTelefono: "02 550271", stato:false, colore: "Giallo"},
+    {immagine: "https://i.ytimg.com/vi/M2jVRUH2JUA/maxresdefault.jpg", nome: "Gabe ", cognome: "Itch", eta: "17", nTelefono: "118", stato: true, colore: "Viola"},
+    {immagine: "https://i.ytimg.com/vi/M2jVRUH2JUA/maxresdefault.jpg", nome: "Dixie " , cognome: "Normus", eta: "18", nTelefono: "1522", stato:false, colore: "Ciano"},
+    {immagine: "https://i.ytimg.com/vi/M2jVRUH2JUA/maxresdefault.jpg", nome: "Antonio", cognome: "Soliman", eta: "15", nTelefono: "+39 348 968 1247", stato:false, colore: "Marrone"},
+    {immagine: "https://i.ytimg.com/vi/M2jVRUH2JUA/maxresdefault.jpg", nome: "Lorenzo " , cognome: "Hu", eta: "18", nTelefono: "+39 347 810 4148", stato:true, colore: "Verde scuro"},
+    {immagine: "https://i.ytimg.com/vi/M2jVRUH2JUA/maxresdefault.jpg", nome: "Thomas ", cognome: "Turbato", eta: "20", nTelefono: "420", stato:false, colore: "f0cd23a"}
   ];
   sfondo = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF', '#800000', '#008000', '#000080']; // Array di colore da inserire come sfondo
 }
