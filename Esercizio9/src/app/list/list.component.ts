71d7760d013388127324a5a917e05a0693c19a74
astorelloo astorelloo <lil.astor06@gmail.com>
2024-04-11 10:50:09 +0000

    un po piu carino

--- Esercizio9/src/app/list/list.component.ts
+++ Esercizio9/src/app/list/list.component.ts
@@ -10,15 +10,10 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
   biglietti=[
+    {immagine:"https://wips.plug.it/cips/tecnologia/cms/2023/12/marte-2.jpg", destinazione:"Marte", sistemaSolare:"Sistema Solare", durata:"0.0000158 anni luce", spazioPorto:"Terra, Spazioporto Internazionale", partenzaArrivo:"Partenza il 12 aprile 2030, Arrivo stimato nell'ottobre 2030", costo:"$200 milioni"},
+    {immagine:"https://upload.wikimedia.org/wikipedia/commons/8/80/Artist%E2%80%99s_impression_of_a_planet_around_Alpha_Centauri_B_%28symbolic%2C_annotated%29.jpg", destinazione:"Alpha Centauri Bb (esopianeta)", sistemaSolare:"Sistema Alpha Centauri", durata:"4.37 anni luce", spazioPorto:"Terra, Spazioporto Internazionale", partenzaArrivo:"Partenza il 1 luglio 2045, Arrivo stimato nel 2050", costo:"$10-12 miliardi"},
     {immagine:"", destinazione:" ", sistemaSolare:"", durata:"", spazioPorto:"", partenzaArrivo:"", costo:""},
     {immagine:"", destinazione:" ", sistemaSolare:"", durata:"", spazioPorto:"", partenzaArrivo:"", costo:""},
     {immagine:"", destinazione:" ", sistemaSolare:"", durata:"", spazioPorto:"", partenzaArrivo:"", costo:""},
-    {immagine:"", destinazione:" ", sistemaSolare:"", durata:"", spazioPorto:"", partenzaArrivo:"", costo:""},
-    {immagine:"", destinazione:" ", sistemaSolare:"", durata:"", spazioPorto:"", partenzaArrivo:"", costo:""},
-    {immagine:"", destinazione:" ", sistemaSolare:"", durata:"", spazioPorto:"", partenzaArrivo:"", costo:""},
-    {immagine:"", destinazione:" ", sistemaSolare:"", durata:"", spazioPorto:"", partenzaArrivo:"", costo:""},
-    {immagine:"", destinazione:" ", sistemaSolare:"", durata:"", spazioPorto:"", partenzaArrivo:"", costo:""},
-    {immagine:"", destinazione:" ", sistemaSolare:"", durata:"", spazioPorto:"", partenzaArrivo:"", costo:""},
-
   ]
 }
