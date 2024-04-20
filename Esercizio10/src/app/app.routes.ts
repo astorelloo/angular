3555592af0ce247d4b541def5960f7c1baf282f6
astorelloo astorelloo <lil.astor06@gmail.com>
2024-04-20 15:42:41 +0000

    home 2 + 0.5 routing

--- Esercizio10/src/app/app.routes.ts
+++ Esercizio10/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { MostreComponent } from './mostre/mostre.component';
+import { HomeComponent } from './home/home.component';
+export const routes: Routes = [
+    {path: 'Mostre', component: MostreComponent},
+    {path: 'Home', component: HomeComponent},  
+  ];
