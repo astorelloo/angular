7194bce00a6c4c72d25baba0f1d358faf7bb891b
astorelloo astorelloo <lil.astor06@gmail.com>
2024-04-21 16:39:48 +0000

    ORA VA:)

--- Esercizio10/src/app/app.routes.ts
+++ Esercizio10/src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { MostreComponent } from './mostre/mostre.component';
 import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
     {path: 'Mostre', component: MostreComponent},
+    {path: '', redirectTo:'/Home', pathMatch: 'full'},
     {path: 'Home', component: HomeComponent},  
   ];
--- Esercizio10/src/app/mostre/mostre.component.ts
+++ Esercizio10/src/app/mostre/mostre.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { DatiService } from '../dati.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mostre',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './mostre.component.html',
   styleUrl: './mostre.component.css'
 })
 export class MostreComponent {
+  constructor(public Dati: DatiService){
 
+  }
 }
